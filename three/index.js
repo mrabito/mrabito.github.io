0ad7af4998e355160c7465c9cebd0ef7d546e926
usagi senm <20224257+mrabito@users.noreply.github.com>
2018-10-10 01:59:48 +0900

    Threejs アニメーションstudy

--- three/index.js
+++ three/index.js
@@ -1,34 +1,57 @@
-var init = function() {
+var mixer = null;
+var clock = new THREE.Clock();
+const scale = 10.0
 
-  //レンダラー作成
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(800, 600);
+// 幅、高さ取得
+const width  = window.innerWidth;
+const height = window.innerHeight;
 
-  //シーン作成
-  document.body.appendChild(renderer.domElement);
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 1000);
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshPhongMaterial({ color: 0x0000ff});
-  var box = new THREE.Mesh(geometry, material);
-  box.position.z = -2
-  scene.add(box);
-  var light = new THREE.DirectionalLight(0xffffff);
-  scene.add(light);
-  light.position.set(1, 1, 1);
+// レンダラの作成、DOMに追加
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+renderer.setClearColor(0xf3f3f3, 1.0);
+document.body.appendChild(renderer.domElement);
 
-  //描画
+// シーンの作成、カメラの作成と追加、ライトの作成と追加
+const scene  = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, width / height, 1, 100 );
+camera.position.set(0, 1, 5);
+const light  = new THREE.AmbientLight(0xffffff, 100);
+scene.add(light);
+var directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
+directionalLight.position.set( 0, 0, 1 );
+scene.add( directionalLight );
+
+const loader = new THREE.GLTFLoader();
+const url = 'model/LabBear_v002.glb';
+
+loader.load(url, (data) => {
+  const gltf = data;
+  const object = gltf.scene;
+  object.scale.set(scale, scale, scale);
+
+  const animations = gltf.animations;
+  mixer = new THREE.AnimationMixer(object);
+  const anime = mixer.clipAction(animations[0]);
+  anime.play();
+  scene.add(object);
+});
+
+// OrbitControls の追加
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.userPan = false;
+controls.userPanSpeed = 0.0;
+controls.maxDistance = 5000.0;
+controls.maxPolarAngle = Math.PI * 0.495;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1.0;
+
+// レンダリング
+const animation = () => {
   renderer.render(scene, camera);
+  controls.update();
+  requestAnimationFrame(animation);
+  if (mixer) mixer.update(clock.getDelta());
+};
 
-  //初回実行
-  var update = function(){
-    requestAnimationFrame(update);
-
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
-
-    renderer.render(scene, camera);
-  }
-  update();
-}
-window.addEventListener('DOMContentLoaded',init);
+animation();
