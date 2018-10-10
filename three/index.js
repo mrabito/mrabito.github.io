89dc8fa0aff89f06ed5b003b564afe9b12f4c4b1
usagi senm <20224257+mrabito@users.noreply.github.com>
2018-10-11 02:39:28 +0900

    three.jsであそんだ

--- three/index.js
+++ three/index.js
@@ -9,13 +9,15 @@ const height = window.innerHeight;
 // レンダラの作成、DOMに追加
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
-renderer.setClearColor(0xf3f3f3, 1.0);
+renderer.setClearColor(0xffffff, 1.0);
 document.body.appendChild(renderer.domElement);
 
 // シーンの作成、カメラの作成と追加、ライトの作成と追加
 const scene  = new THREE.Scene();
+scene.fog = new THREE.Fog(0xffffff, 1, 7);
+
 const camera = new THREE.PerspectiveCamera(50, width / height, 1, 100 );
-camera.position.set(0, 1, 5);
+
 const light  = new THREE.AmbientLight(0xffffff, 100);
 scene.add(light);
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
@@ -38,18 +40,21 @@ loader.load(url, (data) => {
 });
 
 // OrbitControls の追加
+
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.userPan = false;
-controls.userPanSpeed = 0.0;
-controls.maxDistance = 5000.0;
+controls.target.set(0,1,0)
+controls.enablePan = false;
+controls.maxDistance = 6.0;
+controls.minDistance = 3.0;
 controls.maxPolarAngle = Math.PI * 0.495;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 1.0;
+camera.position.set(0, 1, 3.5);
 
 // レンダリング
 const animation = () => {
   renderer.render(scene, camera);
-  controls.update();
+  //controls.update();
   requestAnimationFrame(animation);
   if (mixer) mixer.update(clock.getDelta());
 };
