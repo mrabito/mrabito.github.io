94b8daab1e7a85adbb3f5523bb7cbc0881b1de3a
UsagiSenm <20224257+mrabito@users.noreply.github.com>
2018-10-11 14:06:27 +0900

    プリミティブおいてキャストシャドウした…はずなんだけど、なぜかプリミティブが真っ白になる

--- three/index.js
+++ three/index.js
@@ -11,6 +11,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 renderer.setClearColor(0xffffff, 1.0);
 document.body.appendChild(renderer.domElement);
+renderer.shadowMap.enabled = true;
 
 // シーンの作成、カメラの作成と追加、ライトの作成と追加
 const scene  = new THREE.Scene();
@@ -22,25 +23,30 @@ const light  = new THREE.AmbientLight(0xffffff, 100);
 scene.add(light);
 var directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
 directionalLight.position.set( 0, 0, 1 );
+directionalLight.castShadow = true;
 scene.add( directionalLight );
+
+const meshFloor = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 1, 2),
+  new THREE.MeshStandardMaterial({color: 0x555555, roughness: 0.0}));
+meshFloor.receiveShadow = true;
+scene.add(meshFloor);
 
 const loader = new THREE.GLTFLoader();
 const url = 'model/LabBear_v002.glb';
-
 loader.load(url, (data) => {
   const gltf = data;
   const object = gltf.scene;
   object.scale.set(scale, scale, scale);
-
   const animations = gltf.animations;
   mixer = new THREE.AnimationMixer(object);
   const anime = mixer.clipAction(animations[0]);
   anime.play();
+  object.castShadow = true;
   scene.add(object);
 });
 
 // OrbitControls の追加
-
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.target.set(0,1,0)
 controls.enablePan = false;
